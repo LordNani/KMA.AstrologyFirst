0402662c7aee5b439ae4822602ab853b6b2b8ebc
LordNani <47566320+LordNani@users.noreply.github.com>
2020-03-15 03:14:29 +0200

    Added ChineseeSign calculation, and age check

--- Processors/DataProcessor.cs
+++ Processors/DataProcessor.cs
@@ -8,19 +8,93 @@ namespace pr_1_Gaivoronskyi.Processors
     public int calculateAge(DateTime dateOfBirth)
     {
       if (DateTime.Today.DayOfYear == dateOfBirth.DayOfYear)
-        MessageBox.Show("Happy birthday! ");
+        MessageBox.Show("Happy birthday! ","Oops!",MessageBoxButton.OK, MessageBoxImage.Error);
       int age = DateTime.Today.Year - dateOfBirth.Year;
       if (DateTime.Today.DayOfYear < dateOfBirth.DayOfYear)
         age--;
       // TODO age check
+      if(age > 135 || age < 0)
+            {
+                MessageBox.Show("Wrong age!");
+                return -1;
+            }
       return age;
     }
 
     public String calculateSignEast(DateTime dateOfBirth)
     {
 
-      // TODO age check
-      return "nodata";
+            String result = "";
+            switch (dateOfBirth.Year % 12)
+            {
+                case 0:
+                    {
+                        result = "Monkey";
+                        break;
+                    }
+                case 1:
+                    {
+                        result = "Rooster";
+                        break;
+                    }
+                case 2:
+                    {
+                        result = "Dog";
+                        break;
+                    }
+                case 3:
+                    {
+                        result = "Pig";
+                        break;
+                    }
+                case 4:
+                    {
+                        result = "Rat";
+                        break;
+                    }
+                case 5:
+                    {
+                        result = "Bull";
+                        break;
+                    }
+                case 6:
+                    {
+                        result = "Tiger";
+                        break;
+                    }
+                case 7:
+                    {
+                        result = "Rabbit";
+                        break;
+                    }
+                case 8:
+                    {
+                        result = "Dragon";
+                        break;
+
+                    }
+                case 9:
+                    {
+                        result = "Snake";
+                        break;
+                    }
+                case 10:
+                    {
+                        result = "Horse";
+                        break;
+                    }
+                case 11:
+                    {
+                        result = "Goat";
+                        break;
+                    }
+                default:
+                    {
+                        result = "Can't calculate your sign, sorry";
+                        break;
+                    }
+            }
+            return result;
       // MessageBox.Show($"Date picked: {model.Age}");
     }
 
@@ -39,77 +113,77 @@ namespace pr_1_Gaivoronskyi.Processors
           if (dateOfBirth.Day < 19)
             result = "Aquarius";
           else
-            result = "pisces";
+            result = "Pisces";
           break;
         }
         case 3: {
           if (dateOfBirth.Day < 21)
             result = "Pisces";
           else
-            result = "aries";
+            result = "Aries";
           break;
         }
         case 4: {
           if (dateOfBirth.Day < 20)
             result = "Aries";
           else
-            result = "taurus";
+            result = "Taurus";
           break;
         }
         case 5: {
           if (dateOfBirth.Day < 21)
             result = "Taurus";
           else
-            result = "gemini";
+            result = "Gemini";
           break;
         }
         case 6: {
           if (dateOfBirth.Day < 21)
             result = "Gemini";
           else
-            result = "cancer";
+            result = "Cancer";
           break;
         }
         case 7: {
           if (dateOfBirth.Day < 23)
             result = "Cancer";
           else
-            result = "leo";
+            result = "Leo";
           break;
         }
         case 8: {
           if (dateOfBirth.Day < 23)
             result = "Leo";
           else
-            result = "virgo";
+            result = "Virgo";
           break;
         }
         case 9: {
           if (dateOfBirth.Day < 23)
             result = "Virgo";
           else
-            result = "libra";
+            result = "Libra";
           break;
         }
         case 10: {
           if (dateOfBirth.Day < 23)
             result = "Libra";
           else
-            result = "scorpio";
+            result = "Scorpio";
           break;
         }
         case 11: {
           if (dateOfBirth.Day < 22)
-            result = "scorpio";
+            result = "Scorpio";
           else
-            result = "sagittarius";
+            result = "Sagittarius";
           break;
         }
         case 12: {
           if (dateOfBirth.Day < 22)
             result = "Sagittarius";
           else
-            result = "capricorn";
+            result = "Capricorn";
           break;
         }
         default: {
--- ViewModels/SignViewModel.cs
+++ ViewModels/SignViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows;
 using System;
 using pr_1_Gaivoronskyi.Tools;
 using pr_1_Gaivoronskyi.Models;
@@ -9,96 +8,90 @@ using pr_1_Gaivoronskyi.Processors;
 namespace pr_1_Gaivoronskyi.ViewModels
 {
 
-    internal class SignViewModel : INotifyPropertyChanged
+  internal class SignViewModel : INotifyPropertyChanged
+  {
+#region Fleids
+    private SignModel _model = new SignModel();
+    private DateTime _pickedDate = DateTime.Today;
+    private DataProcessor _processor = new DataProcessor();
+    public RelayCommand<object> _submitDate;
+#endregion
+
+#region Properties
+    public DateTime BirthDate
     {
-        #region Fleids
-        private SignModel _model = new SignModel();
-        private DateTime _pickedDate = DateTime.Today;
-        private DataProcessor _processor = new DataProcessor(); 
-        public RelayCommand<object> _submitDate;
-        #endregion
-
-        #region Properties
-        public DateTime BirthDate
-        {
-            get
-            {
-                return _pickedDate;
-            }
-            set
-            {
-                _pickedDate = value;
-                OnPropertyChanged("BirthDate");
-            }
-        }
-
-        public int Age
-        {
-            get
-            {
-                return _model.Age;
-            }
-            set
-            {
-                _model.Age = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public String EasternSign
-        {
-            get
-            {
-                return _model.EasternSign;
-            }
-            set
-            {
-                OnPropertyChanged("EasternSign");
-            }
-        }
-        public String WesternSign
-        {
-            get
-            {
-                return _model.WesternSign;
-            }
-            set
-            {
-
-                OnPropertyChanged();
-            }
-        }
-
-        public RelayCommand<object> submitDate
-        {
-            get
-            {
-                return _submitDate ?? (_submitDate = new RelayCommand<object>(o =>
-                {
-                    //MessageBox
-                    //    .Show
-                    //        ($"Date picked: {BirthDate}");
-                   Age =  _processor.calculateAge(_pickedDate);
-                }, CanExecuteCommand
-                       ));
-            }
-        }
-
-        #endregion
-
-        public bool CanExecuteCommand(object o)
-        {
-            return !(_pickedDate.Equals(null));
-        }
-
-        #region INotifyPropertyImplementation
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        //[NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
+      get { return _pickedDate; }
+      set
+      {
+        _pickedDate = value;
+        OnPropertyChanged("BirthDate");
+      }
     }
+
+    public int Age
+    {
+      get { return _model.Age; }
+      set
+      {
+        _model.Age = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public String EasternSign
+    {
+      get { return _model.EasternSign; }
+      set
+      {
+        _model.EasternSign = value;
+        OnPropertyChanged();
+      }
+    }
+    public String WesternSign
+    {
+      get { return _model.WesternSign; }
+      set
+      {
+        _model.WesternSign = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public RelayCommand<object> submitDate
+    {
+      get
+      {
+        return _submitDate ??(
+          _submitDate = new RelayCommand<object>(o => {
+            // MessageBox
+            //    .Show
+            //        ($"Date picked: {BirthDate}");
+            int userAge = _processor.calculateAge(_pickedDate);
+            if (userAge != -1) {
+              Age = userAge;
+              WesternSign = _processor.calculateSignWest(_pickedDate);
+              EasternSign = _processor.calculateSignEast(_pickedDate);
+            }
+          }, CanExecuteCommand));
+      }
+    }
+
+#endregion
+
+    public bool CanExecuteCommand(object o)
+    {
+      return !(_pickedDate.Equals(null));
+    }
+
+#region INotifyPropertyImplementation
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    //[NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged(
+      [ CallerMemberName ] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+#endregion
+  }
 }
